bbdb8905958b593f8af2e48a47336f2011799cf6
idanrahamimov <idanrahamimov@gmail.com>
2023-06-21 10:15:48 +0300

    adding excel folder

--- tools/api/analyst.py
+++ tools/api/analyst.py
@@ -1,17 +1,19 @@
 from alpha_vantage_api import get_statement
+import json
 
 class single():
-    def __init__(self, symbol: str, key: str):
+    def __init__(self, symbol: str, key: str, dir_path: str):
+        self.dir_path = dir_path
         self.symbol = symbol
         self.key = key
 
     def create_income_statement(self, quarterly: bool = False):
         df = get_statement(symbol=self.symbol, function='INCOME_STATEMENT', key=self.key, quarterly=quarterly)
-        df.to_excel(f'{self.symbol}_income_statement_{"quarterly" if quarterly else "annual"}.xlsx', index=False)
+        df.to_excel(f'{self.dir_path}/{self.symbol}_income_statement_{"quarterly" if quarterly else "annual"}.xlsx', index=False)
 
     def create_balance_sheet(self, quarterly: bool = False):
         df = get_statement(symbol=self.symbol, function='BALANCE_SHEET', key=self.key, quarterly=quarterly)
-        df.to_excel(f'{self.symbol}_balance_sheet_{"quarterly" if quarterly else "annual"}.xlsx', index=False)
+        df.to_excel(f'{self.dir_path}/{self.symbol}_balance_sheet_{"quarterly" if quarterly else "annual"}.xlsx', index=False)
 
     def get_income_statement(self, quarterly: bool = False):
         df = get_statement(symbol=self.symbol, function='INCOME_STATEMENT', key=self.key, quarterly=quarterly)
@@ -25,3 +27,11 @@ class multiple():
     def __init__(self, symbols: list, key: str):
         self.symbols = symbols
         self.key = key
+
+if __name__ == '__main__':
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    KEY = config['KEY']
+    a = single('AAPL', KEY)
+    a.create_income_statement()
--- tools/api/main.py
+++ tools/api/main.py
@@ -1,14 +1,27 @@
 from analyst import single
 import pandas as pd
 import json
+import os
 
+# Loading the config file
 with open('config.json', 'r') as f:
     config = json.load(f)
 
+# Get api key here https://site.financialmodelingprep.com/developer/docs
+# And put it in the config file
 KEY = config['KEY']
 
+def create_folder(dir_path: str):
+    # Check if the directory already exists
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
 def main():
-    analyst = single(symbol='AAPL',key=KEY)
+    # Create the directory to put the excel files in
+    dir_path='../../excel'
+    create_folder(dir_path=dir_path)
+
+    analyst = single(symbol='AAPL',key=KEY, dir_path=dir_path)
     analyst.create_income_statement()
     analyst.create_income_statement(quarterly=True)
 
